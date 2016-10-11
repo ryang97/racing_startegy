c3d11e2a8fced90f67f099ab8cd5bae0f3267c71
ryang97 <r52yang@uwaterloo.ca>
2016-10-11 00:51:42 -0400

    stintTime module

---
+++ time.cc
@@ -0,0 +1,44 @@
+#include "time.h"
+using namespace std;
+
+Time::Time(int startLap, int endLap, char tyre, float fuelComsumption, float softTime,
+       float medTime, float hardTime):startLap{startLap}, endLap{endLap}, tyre{tyre},
+       fuelConsumption{fuelConsumption}, softTime{softTime}, medTime{medTime},
+       hardTime{hardTime} {}
+
+Time::~Time(){}
+
+
+float Time::softLapTime(int age) { 
+  return age * 0.22;
+}
+
+float Time::medLapTime(int age) { 
+  return age * 0.13;
+}
+
+float Time::hardLapTime(int age) { 
+  return age * 0.05;
+}
+
+float Time::stintTime() {
+  float totalTime = 0;
+  int lap = endLap - startLap;
+  for ( int i = 0; i < lap ; ++i ) {
+    if ((tyre == 'S') or (tyre == 's'))
+      totalTime = totalTime + softTime + softLapTime(i)
+                  - fuelConsumption * (startLap - 1 ) * 0.03; // every 10 kg fuel saves 0.3 
+                                                              // second per lap
+    else if ((tyre == 'M') or (tyre == 'm'))
+      totalTime = totalTime + medTime + medLapTime(i)
+                  - fuelConsumption * (startLap - 1 ) * 0.03;  
+                                                            
+    else if ((tyre == 'H') or (tyre == 'h'))
+      totalTime = totalTime + hardTime + hardLapTime(i)
+                  - fuelConsumption * (startLap - 1 ) * 0.03;
+  }
+  
+  return totalTime;
+}                                                           
+ 
+    
---
+++ time.h
@@ -0,0 +1,24 @@
+#ifndef _TIME_H_
+#define _TIME_H_
+
+
+class Time {
+  int startLap;
+  int endLap;
+  char tyre;
+  float fuelConsumption;
+  //int tyreAge;  
+  float softTime;
+  float medTime;
+  float hardTime;
+  float softLapTime(int age);
+  float medLapTime(int age);
+  float hardLapTime(int age);
+ public:
+  Time(int startLap, int endLap, char tyre, float fuelComsumption, float softTime,
+       float medTime, float hardTime);
+  ~Time();
+  float stintTime();
+};
+    
+#endif
